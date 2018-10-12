f83cb547f968d755a587cff953948a0179794e37
su gyeong <skkim9309@ngle.co.kr>
2018-10-12 19:06:02 +0900

    ServiceImpl 추가

--- springPrj/src/main/java/user/service/UserServiceImpl.java
+++ springPrj/src/main/java/user/service/UserServiceImpl.java
@@ -1,4 +1,108 @@
 package user.service;
 
-public class UserServiceImpl {
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import user.dao.UserDao;
+import user.domain.Level;
+import user.domain.User;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    private DataSource dataSource;
+
+    private MailSender mailSender;
+
+    public static final int MIN_LOGOUT_FOR_SILVER = 50;
+    public  static final int MIN_RECCOMEND_FOR_GOLD = 30;
+    UserDao userDao;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    private PlatformTransactionManager transactionManager;
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    public void setMailSender(MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void upgradeLevels() {
+
+        List<User> users = userDao.getAll();
+        for (User user : users) {
+            if (canUpgradeLevel(user)) {
+                upgradeLevel(user);
+            }
+        }
+
+        /*TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            upgradeLevelsInternal();
+
+            this.transactionManager.commit(status);
+        } catch (Exception e) {
+            this.transactionManager.rollback(status);
+        }*/
+
+    }
+
+    private void upgradeLevelsInternal() {
+        List<User> users = userDao.getAll();
+        for (User user : users) {
+            if (canUpgradeLevel(user)) {
+                upgradeLevel(user);
+            }
+        }
+    }
+
+    protected void upgradeLevel(User user) {
+        user.upgradeLevel();
+        userDao.update(user);
+        sendUpgradeEmail(user);
+    }
+
+    private void sendUpgradeEmail(User user) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(user.getEmail());
+        mailMessage.setFrom("skkim9309@naver.com");
+        mailMessage.setSubject("Upgrade 안내");
+        mailMessage.setText("사용자님의 등급이" + user.getLevel().name());
+
+        this.mailSender.send(mailMessage);
+    }
+
+    private boolean canUpgradeLevel(User user) {
+        Level currentLevel = user.getLevel();
+        switch (currentLevel) {
+            case BASIC:
+                return (user.getLogin() >= MIN_LOGOUT_FOR_SILVER);
+            case SILVER:
+                return (user.getRecommend() >= MIN_RECCOMEND_FOR_GOLD);
+            case GOLD:
+                return false;
+            default:
+                throw new IllegalArgumentException("Unknown Level : " + currentLevel);
+        }
+    }
+
+    public void add(User user) {
+        if (user.getLevel() == null) {
+            user.setLevel(Level.BASIC);
+        }
+        userDao.add(user);
+    }
 }
